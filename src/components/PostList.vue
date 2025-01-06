eb5abec73e1f8c6c5964a6556ae90688f2413985
front_academy <dmitrypetrakevich@gmail.com>
2025-01-06 22:29:03 +0700

    Добавил анимацию для списка

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,15 @@
 <template>
     <div v-if="posts.length > 0">
         <h3>Список поcтов</h3>
-        <post-item 
+        <transition-group name="post-list">
+            <post-item 
         v-for="post in posts" 
         :post="post"
         :key="post.id"
         @remove="$emit('remove', post)"
         />
+        </transition-group>
+       
     </div>
 
     <h2 v-else style="color: red;">
@@ -38,5 +41,27 @@ export default {
 
 
 <style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
 
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+.post-list-enter-from {
+  opacity: 0;
+  transform: translateY(20px);
+  filter: blur(10px);
+}
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+  filter: blur(10px);
+}
+
+.post-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
