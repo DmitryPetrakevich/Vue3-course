d55d23f9da2f8786f66f313f4198afbf9506ffed
front_academy <dmitrypetrakevich@gmail.com>
2025-01-15 21:01:02 +0700

    обавил папку pages и пару страниц. обавил снипет

---
+++ src/pages/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Добро пожалоть на курс по Vue 3</h1>
+    </div>
+
+</template>
+
+
+
+<script>
+
+
+</script>
+
+
+
+<style scoped>
+
+
+
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,19 @@
+import Main from '@/pages/Main.vue'
+import { createRouter, createWebHistory } from "vue-router"
+
+
+
+const routes = [
+    {
+        path: '/',
+        component: Main
+    }
+]
+
+
+const router = createRouter( {
+    routes,
+    history: createWebHistory(process.env.BASE_URL)
+})
+
+export default router;
